9957366be6a0ec7a2ab1d657f32113424125f0df
Judy <judychue@pursuit.org>
2023-04-03 17:57:06 -0400

    Judy-Portfolio-Lab_stage-4

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ const options = {
 
 fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planets/', options)
 	.then(response => response.json())
-	.then(response => displayPlanet(response))
+	.then(response => {
+        console.log("enter a planet name")
+        displayPlanet(response)
+    })
 	.catch(err => console.error(err));
 
 function displayPlanet(planets){
+    console.log("display planet logged");
     let image=document.querySelector("img")
     image.src=planets[2].imgSrc.img;
     image.alt=planets[2].imgSrc.imgDescription;
@@ -25,15 +29,24 @@ function displayPlanet(planets){
         const planetInput = document.getElementById("planetInput");
         let find=planetInput.value;
         planetInput.value=""
+        let planetFound = false
         for (const planet of planets) {
             if (planet.name.toUpperCase()===find.toUpperCase()){
+                planetFound = true
                 image.src=planet.imgSrc.img
                 image.alt=planet.imgSrc.imgDescription;
                 planetDescription.textContent=planet.description;
 
             }
         }
+        
+        if (!planetFound){
+          window.alert("error!, enter planet name")
+        }
 
     })
 
 }
+
+// set time interval or set time out set a class to what is changing
+// ex: drk blue- .css changes font color
